a0b4f00376cb65bfdc84c277e07d723974df8dad
Alon Hammerman <the.alonso@gmail.com>
2013-03-12 19:38:08 +0200

    Photo - add meaningful unicode repr

--- photo_album/models.py
+++ photo_album/models.py
@@ -5,3 +5,10 @@ class Photo(models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200, blank=True)
     image = CloudinaryField('image')
+
+    def __unicode__(self):
+        try:
+            public_id = self.image.public_id
+        except AttributeError:
+            public_id = ''
+        return "Photo <%s:%s>" % (self.title, public_id)
